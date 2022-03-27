233b77860e71814011c613267acf9be2bafed7e8
Glaudston Martins <glaudstonalan@gmail.com>
2022-03-27 17:57:04 -0300

    js

---
+++ enviar.js
@@ -0,0 +1,8 @@
+function enviar(){
+var nome = document.getElementById('nome').value;
+var email = document.getElementById ('email').value
+var destino = 'glaudstonalan@gmail.com '
+
+alert(`nome: ${nome},  E-mail : ${email} `);
+console.log('ola')
+}
