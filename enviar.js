c58ab982ebba2ec92129f4a5dc29ae37619624a2
Glaudston Martins <glaudstonalan@gmail.com>
2022-04-02 14:17:54 -0300

    ajuste nome img logo.png

--- enviar.js
+++ enviar.js
@@ -1,8 +1,15 @@
+
 function enviar(){
-var nome = document.getElementById('nome').value;
-var email = document.getElementById ('email').value
-var destino = 'glaudstonalan@gmail.com '
+    var nome = document.getElementById('nome').value;
+    var emailDestinoUsuario = document.getElementById('email').value;
 
-alert(`nome: ${nome},  E-mail : ${email} `);
+    var emailDestinoSemeia = "glaudstonalan@gmail.com";
 
+    //email enviado para o glaudstonalan@gmail.com
+    MailApp.sendEmail(emailDestinoSemeia, assunto, emailBody);
+
+    //email enviado para o usuário que se cadastrou para receber as novidades
+    MailApp.sendEmail(emailDestinoUsuario, assunto, emailBody);
+
+    document.alert("Olá "+nome+" Semeia Flores e Plantas agradece seu interesse. Em breve você receberá mais informações sobre as novidades!");
 }
